0a3ba32ac67407e14f34beff0886b21334c8f37b
Yateen00 <codingdudelolhow@gmail.com>
2025-02-14 00:53:27 +0530

    added user with devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :username, presence: true, uniqueness: true
+
+  # Additional model code...
+end
