9af8f65585f9475e7d159037430a311ae9d3aa46
Yateen00 <codingdudelolhow@gmail.com>
2025-03-02 14:11:27 +0530

    changed calender to droplet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_23_182336) do
+ActiveRecord::Schema[8.0].define(version: 20_250_223_182_336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_182336) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["followee_id"], name: "index_follows_on_followee_id"
-    t.index ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true
+    t.index %w[follower_id followee_id], name: "index_follows_on_follower_id_and_followee_id", unique: true
     t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_182336) do
     t.bigint "likeable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable"
+    t.index %w[likeable_type likeable_id], name: "index_likes_on_likeable"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_182336) do
     t.boolean "read", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index %w[notifiable_type notifiable_id], name: "index_notifications_on_notifiable"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
@@ -75,12 +75,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_182336) do
     t.integer "comments_count", default: 0
     t.integer "likes_count", default: 0
     t.index ["creator_id"], name: "index_posts_on_creator_id"
-    t.index ["postable_type", "postable_id"], name: "index_posts_on_postable"
+    t.index %w[postable_type postable_id], name: "index_posts_on_postable"
   end
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.integer "gender", default: 0
+    t.integer "gender", default: 4
     t.string "link"
     t.text "bio"
     t.string "profile_picture"
